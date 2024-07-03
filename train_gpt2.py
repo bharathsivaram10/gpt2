028a7fe971e53da3b5d3ff77bffd512751e0c79a
Bharath Sivaram <bharathsivaram10@gmail.com>
2024-07-02 23:07:02 -0500

    first commit

---
+++ train_gpt2.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embd: int = 384
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self,config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+
+        # key, query, value projection for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # not really 'bias', more of a mask, but following OpenAI/HF naming
+        self.register_buffer("bias")
+
+
+
+    def forward(self,x):
+        B, T, C = x.shape # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, vals for all heads in batch and move head forward to be the batch
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_heads = 12, hs = 64, so nh * hs = 768 channels in Transformer    
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
+
+        # Attention (materializes the large (T,T) matrix for all the queries and keys)
+        att = 
+
+
+
+
+class MLP(nn.Module):
+
+    def __init__(self,config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh') # historical artifact due to erf being slow
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        
+    def forward(self,x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x - self.c_proj(x)
+
+        return x
+
+
+class Block(nn.Module):
+
+    def __init__(self,config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self,x):
+        x = x + self.attn(self.ln_1(x)) # reduce
+        x = x + self.mlp(self.ln_2(x)) # map
+
+        # tranformer is like map reduce in a certain way :)
+
+        return x
+
+class GPT(nn.Module):
+
+    def __init__(self,config):
+
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict{
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList(Block(config) for _ in range(config.n_layer)),
+            ln_f = nn.LayerNorm(config.n_embd)
+
+        })
+
+        self.lm_head = nn.Linear(config.n_emdb, config.vocab_size, bias=False)
